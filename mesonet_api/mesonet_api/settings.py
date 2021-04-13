3c0a632e3f0cf30b425669a463893a8cbcc5cd94
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-04-13 21:32:11 +0530

    Fix

--- mesonet_api/mesonet_api/settings.py
+++ mesonet_api/mesonet_api/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "puw@bv1)j^0a@(%_lgyhzf=a37wd=((mcykbuw*c7pcbe@+cv@"
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
