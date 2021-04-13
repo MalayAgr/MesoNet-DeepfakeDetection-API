0390560553ec00893a0d5a1a09f7812125abebb1
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-04-13 19:35:16 +0530

    Add favicon

--- mesonet_api/mesonet_api/urls.py
+++ mesonet_api/mesonet_api/urls.py
@@ -19,15 +19,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/', include('api.urls')),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path("api/", include("api.urls")),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static("/favicon.ico", document_root="media/favicon.ico")
